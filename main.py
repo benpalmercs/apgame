a7b081c9b2d930581ca838443c1106fd5a8da028
Ben Palmer <bencharlespalmer@gmail.com>
2024-02-15 10:34:29 -0600

    gravity and walls

--- main.py
+++ main.py
@@ -5,24 +5,46 @@ import math
 ### CLASSES ###
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self,color):
         super(Player,self).__init__()
         self.x = 500
-        self.y = 300
+        self.y = 500
         self.image = pygame.Surface((25,50), pygame.SRCALPHA, 32)
-        self.image.fill(RED)
+        self.image.fill(color)
         self.rect = self.image.get_rect(center=(self.x,self.y))
+        self.jump_count = 0
 
     def move(self,deltax,deltay):
         self.rect.centerx += deltax
         self.rect.centery += deltay
+
+    def gravity(self):
+        self.y += 5
+        self.rect.centery = self.y
+        if self.rect.centery > 600:
+            self.kill()
+            print("Death")
+    
+    def jump(self):
+        if self.jump_count < 20:
+            self.y -= 10
+            self.rect.centery = self.y
+        self.jump_count += 1
+    
+    def on_ground(self,group):
+        for item in group:
+            if (item.rect.top < (self.rect.bottom + 10)) and (item.rect.top > (self.rect.bottom - 10)) and (item.rect.left < self.rect.right) and (item.rect.right > self.rect.left):
+                return True
+
 
 class Wall(pygame.sprite.Sprite):
     def __init__(self,x,y,width,length):
         super(Wall,self).__init__()
         self.x = x
         self.y = y
-        self.image = pygame.surface((width,length), pygame.SRCALPHA, 32)
+        self.image = pygame.Surface((width,length), pygame.SRCALPHA, 32 )
+        self.image.fill(BLACK)
+        self.rect = self.image.get_rect(center=(self.x,self.y))
 
 
 
@@ -47,14 +69,35 @@ BLACK = (0, 0, 0)
 RED = (255,0,0)
 GREEN = (0,255,0)
 SKY = (85,156,187)
+PURPLE = (146,79,171)
+PINK = (255,0,221)
+LIGHT_BLUE = (0,255,255)
+FOREST_GREEN = (28,122,78)
 
 # Create clock to later control frame rate
 clock = pygame.time.Clock()
 
 # SPRITE GROUPS #
-player = Player()
+player1 = Player(FOREST_GREEN)
+player2 = Player(PURPLE)
 players = pygame.sprite.Group()
-players.add(player)
+players.add(player1)
+players.add(player2)
+
+walls = pygame.sprite.Group()
+floor = Wall(500,580,3000,50)
+walls.add(floor)
+
+#making more walls
+wallx = 50
+wally = 500
+for i in range(3):
+    walls.add(Wall(wallx,wally,60,10))
+    wally -= 90
+    wallx += 140
+    walls.add(Wall(wallx,wally,60,10))
+    wally -= 90
+    wallx -= 140
 
 
 
@@ -76,19 +119,37 @@ while running:
     screen.fill(WHITE)
 
     players.draw(screen)
+    walls.draw(screen)
 
     keys = pygame.key.get_pressed()
+
+    # PLAYER 1 CONTROLS #
     if keys[pygame.K_UP]:
-        player.move(0,-8)
-        
-    if keys[pygame.K_DOWN]:
-        player.move(0,8)
+        player1.jump()
 
     if keys[pygame.K_LEFT]:
-        player.move(-8,0)
+        player1.move(-8,0)
         
     if keys[pygame.K_RIGHT]:
-        player.move(8,0)
+        player1.move(8,0)
+
+    # PLAYER 2 CONTROLS #
+    if keys[pygame.K_w]:
+        player2.jump()
+
+    if keys[pygame.K_a]:
+        player2.move(-8,0)
+        
+    if keys[pygame.K_d]:
+        player2.move(8,0)
+
+    for player in players:
+        # on_ground = pygame.sprite.spritecollide(player,walls,False) #spritecollide function idea taken from https://stackoverflow.com/questions/43474849/pygame-sprite-collision-with-sprite-group
+         
+        if not player.on_ground(walls):
+            player.gravity()
+        if player.on_ground(walls):
+            player.jump_count = 0
     # Update the display
     pygame.display.flip()
 
